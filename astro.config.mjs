c5152a935ae86d0fa8cec0447a9bef25c52e07d8
sorphil <psoriano01@hotmail.com>
2025-04-05 23:31:46 -0400

    added sitemap and meta information

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://taylorrenegade.ca',
+    integrations: [sitemap(
+      {
+        filter: (page) => {
+          // Exclude specific paths
+          if (
+            page.includes('/other-works/ow') ||
+            page.includes('/forensic-art/fa')
+          ) {
+            return false;
+          }
+          return true;
+      }
+    }
+    )],
+  });
